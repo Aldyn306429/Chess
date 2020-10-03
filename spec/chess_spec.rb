7e57b28aabd7265340c2471a09b4173f9fbf721d
Aldyn Chen <Aldynchen1234@gmail.com>
2020-10-02 18:46:47 -0700

    Build foundation for tests

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,5 +1,61 @@
 describe 'Tests' do
-  context 'somefolder' do
-      # to be determined
+  context 'Pieces' do
+    context 'Bishop' do
+      context '#possible_moves' do
+        it 'message' do
+          
+        end
+      end
+    end
+    context 'King.rb' do
+      context '#possible_moves' do
+        it 'message' do
+          
+        end
+      end
+    end
+    context 'Knight.rb' do
+      context '#possible_moves' do
+        it 'message' do
+          
+        end
+      end
+    end
+    context 'Pawn.rb' do
+      context '#possible_moves' do
+        it 'message' do
+          
+        end
+      end
+    end
+    context 'Queen.rb' do
+      context '#possible_moves' do
+        it 'message' do
+          
+        end
+      end
+    end
+    context 'Rook.rb' do
+      context '#possible_moves' do
+        it 'message' do
+          
+        end
+      end
+    end
+  end
+  context 'Board.rb' do
+    
+  end
+  context 'Chess.rb' do
+    
+  end
+  context 'Game_master.rb' do
+    
+  end
+  context 'Player.rb' do
+    
+  end
+  context 'Save.rb' do
+    
   end
 end
